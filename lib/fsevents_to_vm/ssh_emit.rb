96bcf8e4f608a6b49ec79db320237c1f6501ecb3
Brian Palmer <brianp@instructure.com>
2015-11-03 14:52:28 -0700

    resilience to ssh disconnect

--- lib/fsevents_to_vm/ssh_emit.rb
+++ lib/fsevents_to_vm/ssh_emit.rb
@@ -5,11 +5,25 @@ require 'tempfile'
 module FseventsToVm
   class SshEmit
     def initialize(ssh_config_file)
-      @ssh = Net::SSH.start('dinghy', 'docker', config: ssh_config_file.path)
+      @config_path = ssh_config_file.path
     end
 
     def event(event)
-      @ssh.exec!("touch -m -c -t #{event.mtime} #{Shellwords.escape event.path}".force_encoding(Encoding::BINARY))
+      ssh.exec!("touch -m -c -t #{event.mtime} #{Shellwords.escape event.path}".force_encoding(Encoding::BINARY))
+    rescue SystemCallError => e
+      $stderr.puts "Error sending event: #{e}"
+      $stderr.puts "\t#{e.backtrace.join("\n\t")}"
+      disconnect!
+    end
+
+    protected
+
+    def ssh
+      @ssh ||= Net::SSH.start('dinghy', 'docker', config: @config_path)
+    end
+
+    def disconnect!
+      @ssh = nil
     end
   end
 end
