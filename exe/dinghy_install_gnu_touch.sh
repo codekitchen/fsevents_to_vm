2ccbe4aad30f6ba9031d15b7ef801876a998279c
Marc Siegel <marc@usainnov.com>
2018-05-29 13:48:12 -0400

    PR review 1: copy 'touch' from dinghy-http-proxy instead of ubuntu

--- exe/dinghy_install_gnu_touch.sh
+++ exe/dinghy_install_gnu_touch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ##
-# Extract the 'touch' binary from the Ubuntu docker container,
+# Extract the 'touch' binary from the 'dinghy-http-proxy' Docker container,
 # and install it as '/bin/gtouch' inside the Dinghy boot2docker VM.
 #
 # This binary is compatible with the VM's BusyBox image, and provides
@@ -8,18 +8,18 @@
 ##
 set -ex
 
-# In order to copy from the Ubuntu docker container to a host temporary
+# In order to copy from a Docker container to a host temporary
 # directory, it must be under the directory tree exported via Dinghy's
 # NFS server into the VM.
 HOST_NFS_DIR="${DINGHY_HOST_MOUNT_DIR:-${HOME}}"
 mkdir -pv "${HOST_NFS_DIR}/tmp"
 HOST_TMP_DIR=$(mktemp -d "${HOST_NFS_DIR}/tmp/dinghy_tmp.XXXXXXXX")
 
-# Copy 'touch' from a Ubuntu container to host
-docker run -v ${HOST_TMP_DIR}:/host_tmp_dir ubuntu:16.04 \
-       /bin/cp /usr/bin/touch /host_tmp_dir/
+# Copy 'touch' from a Linux Docker container to the host
+docker run -v ${HOST_TMP_DIR}:/host_tmp_dir codekitchen/dinghy-http-proxy:2.5 \
+       /bin/cp /bin/touch /host_tmp_dir/
 
-# Copy 'touch' from host to Dinghy VM as 'gtouch'
+# Copy 'touch' from host to the Dinghy VM as 'gtouch'
 dinghy ssh sudo cp ${HOST_TMP_DIR}/touch /bin/gtouch
 
 # Cleanup host temporary directory
